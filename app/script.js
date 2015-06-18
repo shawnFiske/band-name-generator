5b0ce0457c0ea656d20407ebc1d0afbf50174db2
Shawn <shawn@shawn.dev.laptop>
2015-06-18 10:42:40 -0700

    updated layout and messaging

--- app/script.js
+++ app/script.js
@@ -2,61 +2,69 @@
 $('#getBandName').on('click',function(){
   $.get("http://localhost:3000/adjective", function(response){
     var adjective = response.word;
-    $('#adjective').text(adjective);
+    $('#adjective').text(capitalizeWord(adjective));
   });
 
   $.get("http://localhost:3000/verb", function(response){
     var verb = response.word;
-    $('#verb').text(verb);
+    $('#verb').text(capitalizeWord(verb));
   });
 
   $.get("http://localhost:3000/noun", function(response){
     var noun = response.word;
-    $('#noun').text(noun);
+    $('#noun').text(capitalizeWord(noun));
   });
+
+  $('input[".textBox"]').val('');
 });
 
-$('#submitAdjective').on('submit', function(e){
+function capitalizeWord(word){
+  var newWord = word;
+  newWord = newWord.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+    return letter.toUpperCase();
+  });
+  
+  return newWord;
+}
+
+$('#submitWords').on('submit', function(e){
   e.preventDefault();
   var adjective = $('input[name=adjective]').val();
   var adjPost;
+
+  var verb = $('input[name=verb]').val();
+  var verbPost;
+
+  var noun = $('input[name=noun]').val();
+  var nounPost;
+
+  //console.log(adjective +" : "+ verb +" : "+ noun)
 
   if(adjective){
     adjPost = {word: adjective};
     $.post('adjective', adjPost, function(response){
       var adjectiveRes = response.msg;
-      $('#wordRes').text(adjectiveRes)
+      $('#wordAdj').text(adjectiveRes);
+      $('input[name=adjective]').val('');
     });
   }
-    //console.log(adjectiveRes);
-});
-
-$('#submitVerb').on('submit', function(e){
-  e.preventDefault();
-  var verb = $('input[name=verb]').val();
-  var verbPost;
 
   if(verb){
     verbPost = {word: verb};
     $.post('verb', verbPost, function(response){
       var verbRes = response.msg;
-      $('#wordRes').text(verbRes)
+      $('#wordVerb').text(verbRes);
+      $('input[name=verb]').val('');
     });
   }
-    //console.log(verbRes);
-});
-
-$('#submitNoun').on('submit', function(e){
-  e.preventDefault();
-  var noun = $('input[name=noun]').val();
-  var nounPost;
 
   if(noun){
     nounPost = {word: noun};
     $.post('noun', nounPost, function(response){
       var nounRes = response.msg;
-      $('#wordRes').text(nounRes)
+      $('#wordNoun').text(nounRes);
+      $('input[name=noun]').val('');
     });
   }
-    //console.log(adjectiveRes);
+  //console.log(adjectiveRes);
 });
