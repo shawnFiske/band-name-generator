b9ffac56300e4a893b5f4137a38d41f175c1c34e
Shawn <shawn@shawn.dev.laptop>
2015-06-18 19:07:09 -0700

    update after fixing grunt errors

--- app/script.js
+++ app/script.js
@@ -1,16 +1,21 @@
+'use strict';
 
-$('#getBandName').on('click',function(){
-  $.get("http://localhost:3000/adjective", function(response){
+$('#getBandName').on('click', function() {
+
+  $.get('http://localhost:3000/adjective', function(response) {
+
     var adjective = response.word;
     $('#adjective').text(capitalizeWord(adjective));
   });
 
-  $.get("http://localhost:3000/verb", function(response){
+  $.get('http://localhost:3000/verb', function(response) {
+
     var verb = response.word;
     $('#verb').text(capitalizeWord(verb));
   });
 
-  $.get("http://localhost:3000/noun", function(response){
+  $.get('http://localhost:3000/noun', function(response) {
+
     var noun = response.word;
     $('#noun').text(capitalizeWord(noun));
   });
@@ -18,17 +23,19 @@ $('#getBandName').on('click',function(){
   $('input[".textBox"]').val('');
 });
 
-function capitalizeWord(word){
+function capitalizeWord(word) {
+
   var newWord = word;
   newWord = newWord.toLowerCase().replace(/\b[a-z]/g, function(letter) {
     return letter.toUpperCase();
   });
-  
+
   return newWord;
 }
 
-$('#submitWords').on('submit', function(e){
+$('#submitWords').on('submit', function(e) {
   e.preventDefault();
+
   var adjective = $('input[name=adjective]').val();
   var adjPost;
 
@@ -41,31 +48,39 @@ $('#submitWords').on('submit', function(e){
   //console.log(adjective +" : "+ verb +" : "+ noun)
   $('#msg p').empty();
 
-  if(adjective){
+  if (adjective) {
+
     adjPost = {word: adjective};
-    $.post('adjective', adjPost, function(response){
+
+    $.post('adjective', adjPost, function(response) {
       var adjectiveRes = response.msg;
       $('#wordAdj').text(adjectiveRes);
       $('input[name=adjective]').val('');
     });
   }
 
-  if(verb){
+  if (verb) {
+
     verbPost = {word: verb};
-    $.post('verb', verbPost, function(response){
+    $.post('verb', verbPost, function(response) {
+
       var verbRes = response.msg;
       $('#wordVerb').text(verbRes);
       $('input[name=verb]').val('');
+
     });
   }
 
-  if(noun){
+  if (noun) {
     nounPost = {word: noun};
-    $.post('noun', nounPost, function(response){
+    $.post('noun', nounPost, function(response) {
+
       var nounRes = response.msg;
       $('#wordNoun').text(nounRes);
       $('input[name=noun]').val('');
+
     });
   }
+
   //console.log(adjectiveRes);
 });
--- server.js
+++ server.js
@@ -28,44 +28,44 @@ var noun      = new Noun();
 
 //end points
 //root end point
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
 
 //adjective end point
-app.get('/adjective', function(req, res){
+app.get('/adjective', function(req, res) {
   res.json(getRandomWord(adjective));
 });
 
 //verb end point
-app.get('/verb', function(req, res){
+app.get('/verb', function(req, res) {
   res.json(getRandomWord(verb));
 });
 
 //noun end point
-app.get('/noun', function(req, res){
+app.get('/noun', function(req, res) {
   res.json(getRandomWord(noun));
 });
 
 //add adjective end point
-app.post('/adjective', function(req, res){
+app.post('/adjective', function(req, res) {
   var word = postRandomWord(req.body.word, adjective);
   res.json(word);
 });
 
 //add verb end point
-app.post('/verb', function(req, res){
+app.post('/verb', function(req, res) {
   var word = postRandomWord(req.body.word, verb);
   res.json(word);
 });
 
 //add noun end point
-app.post('/noun', function(req, res){
+app.post('/noun', function(req, res) {
   var word = postRandomWord(req.body.word, noun);
   res.json(word);
 });
 
 //listen for requests on port
-app.listen(port, function(){
+app.listen(port, function() {
   console.log('Server available at localhost: ' + port);
 });
